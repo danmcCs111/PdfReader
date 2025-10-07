0ec05bf0a39fd397a41ac2e0936e88260820a0e4
danmcCs111 <danmccs111@gmail.com>
2025-10-07 11:49:23 -0400

    fix by removing loop. write plain text to file

--- src/main/java/PdfReader/PdfReader.java
+++ src/main/java/PdfReader/PdfReader.java
@@ -1,6 +1,7 @@
 package PdfReader;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import org.apache.pdfbox.Loader;
@@ -9,9 +10,14 @@ import org.apache.pdfbox.text.PDFTextStripper;
 
 public class PdfReader 
 {
+	
+	private static final String 
+		PDF_FILENAME = "./my_sql-refman-8.4-en.a4.pdf",
+		TEXT_FILENAME = "./mysql-refman-text.txt";
+	
 	public static void main(String [] args)
 	{
-		File file = new File("./my_sql-refman-8.4-en.a4.pdf");
+		File file = new File(PDF_FILENAME);
 		try {
 			PDDocument document = Loader.loadPDF(file);
 			System.out.println("PDF loaded"); 
@@ -24,16 +30,24 @@ public class PdfReader
 	
 	private static void printPages(PDDocument document)
 	{
-		for(int i = 0; i < document.getNumberOfPages(); i++)
-		{
-//			PDPage pdPage = document.getPage(i);
-			try {
-				PDFTextStripper pdfStripper = new PDFTextStripper();
-				String text = pdfStripper.getText(document);
-				System.out.println(text);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		try {
+			PDFTextStripper pdfStripper = new PDFTextStripper();
+			String text = pdfStripper.getText(document);
+			writeToFile(text, TEXT_FILENAME);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void writeToFile(String content, String textFilename)
+	{
+		try {
+			FileWriter writer = new FileWriter(textFilename);
+			writer.write(content); // Write content to the file
+			writer.close(); // Close the writer to release resources
+			System.out.println("Successfully wrote to the file.");
+		} catch (IOException e) {
+			System.out.println("An error occurred: " + e.getMessage());
 		}
 	}
 	
